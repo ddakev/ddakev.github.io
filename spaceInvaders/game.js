d6ebe52b19b184f1525ccb450b64db1201e75f97
Daniel Dakev <ddakev@gmail.com>
2016-04-12 23:27:32 -0400

    SpaceInvaders enemies update
    Including enemies in 5 rows and 7 columns, each row with a different
    picture and hopefully, different armor.

--- spaceInvaders/game.js
+++ spaceInvaders/game.js
@@ -25,7 +25,8 @@ var canvas,
     player,
     background,
     keysPressed = new Array(223),
-    bullets = [];
+    bullets = [],
+    enemies = [];
 
 function Background() {
     this.width = 1366;
@@ -57,8 +58,8 @@ function Player(x,y) {
     this.speedY = 0;
     this.acceleration = 0.2;
     this.maxSpeed = 4;
-    this.width = 42;
-    this.height = 44;
+    this.width = 41;
+    this.height = 46;
     this.image = new Image();
     this.image.src = "images/spaceshipSprite.png";
     this.imageX = 0;
@@ -101,9 +102,9 @@ Player.prototype.moveBy = function(dX, dY) {
         this.speedY += this.acceleration * dY / Math.sqrt(dX*dX + dY*dY);
     }
     if(Math.sqrt(this.speedX*this.speedX + this.speedY*this.speedY) > this.maxSpeed) {
-        if(this.dX != 0 && Math.abs(this.speedX) < Math.abs(this.speedY))
+        if(dX != 0 && Math.abs(this.speedX) < Math.abs(this.speedY))
             this.speedX += this.acceleration * dX / Math.sqrt(dX*dX + dY*dY);
-        else if(this.dY != 0 && Math.abs(this.speedY) < Math.abs(this.speedX))
+        else if(dY != 0 && Math.abs(this.speedY) < Math.abs(this.speedX))
             this.speedY += this.acceleration * dY / Math.sqrt(dX*dX + dY*dY);
         this.speedX = this.maxSpeed*this.speedX/Math.sqrt(this.speedX*this.speedX + this.speedY*this.speedY);
         this.speedY = this.maxSpeed*this.speedY/Math.sqrt(this.speedX*this.speedX + this.speedY*this.speedY);
@@ -122,12 +123,12 @@ Player.prototype.draw = function(ctx) {
                   this.x, this.y, this.width, this.height);
 };
 
-function Bullet(x,y) {
+function Bullet(x,y,type) {
     this.x = x;
     this.y = y;
-    this.width = 3;
-    this.height = 9;
-    this.speed = -4;
+    this.width = 4;
+    this.height = 12;
+    this.speed = 4*(type==0?-1:1);
     this.image = new Image();
     this.image.src = "images/alt_bullet.png";
 }
@@ -142,6 +143,54 @@ Bullet.prototype.draw = function(ctx) {
     ctx.drawImage(this.image, this.x, this.y, this.width, this.height);
 };
 
+function Enemy(type,x,y) {
+    this.x = x;
+    this.y = y;
+    this.startY = y;
+    this.changeY = 40;
+    this.type = type;
+    this.width = 72;
+    this.height = 50;
+    this.speed = 0.5;
+    this.direction = 0; //0 for right, 1 for down, 2 for left, 3 for down again
+    this.armor = type+1;
+    this.image = new Image();
+    this.image.src = "images/enemies.png";
+}
+Enemy.prototype.move = function() {
+    if(this.direction == 0) {
+        this.x += this.speed;
+    }
+    else if(this.direction == 2) {
+        this.x -= this.speed;
+    }
+    else {
+        this.y += this.speed;
+        if(this.y-this.startY > this.changeY) {
+            this.y = this.startY + this.changeY;
+            this.startY = this.y;
+            this.direction = (this.direction+1)%4;
+        }
+    }
+    
+};
+Enemy.prototype.moveBy = function(dX, dY) {
+    this.x += dX;
+    this.y += dY;
+};
+Enemy.prototype.draw = function(ctx) {
+    ctx.drawImage(this.image, this.type*this.image.width/5, 0, this.image.width/5, this.image.height,
+                  this.x, this.y, this.width, this.height);
+};
+
+function spawnEnemies() {
+    for(var i = 0; i < 5; i++) {
+        for(var j = 0; j < 7; j++) {
+            enemies.push(new Enemy(i, 0.15*screenWidth+j*(0.7*screenWidth-72)/6, 0.05*screenHeight+i*0.1*screenHeight));
+        }
+    }
+}
+
 function initialize() {
     for(var i=0; i<223; i++)
         keysPressed[i] = false;
@@ -150,9 +199,10 @@ function initialize() {
     
     canvas = document.getElementById("gameWindow");
     ctx = canvas.getContext('2d');
+    ctx.fillStyle="#fff";
     player = new Player(673,550);
     background = new Background();
-    ctx.fillStyle="#fff";
+    spawnEnemies();
     
     main(0);
 }
@@ -174,9 +224,26 @@ function update(delta) {
     else
         player.moveBy(0,0);
     
+    var shouldChangeDir = 0;
+    enemies.forEach(function(enemy) {
+        enemy.move();
+        if(shouldChangeDir == 0 && enemy.x > 0.95*screenWidth - 72) {
+            shouldChangeDir = enemy.x - 0.95*screenWidth + 72;
+        }
+        if(shouldChangeDir == 0 && enemy.x < 0.05*screenWidth) {
+            shouldChangeDir = enemy.x - 0.05*screenWidth;
+        }
+    });
+    if(shouldChangeDir!=0) {
+        enemies.forEach(function(enemy) {
+            enemy.x -= shouldChangeDir;
+            enemy.direction = (enemy.direction+1)%4;
+        });
+    }
+    
     if(keysPressed[KEY_SPACE]) {
         if(lastFrame - lastBullet >= BULLET_COOLDOWN) {
-            bullets.push(new Bullet(player.x + player.width/2 - 3/2, player.y));
+            bullets.push(new Bullet(player.x + player.width/2 - 3/2, player.y, 0));
             lastBullet = lastFrame;
         }
     }
@@ -202,9 +269,10 @@ function draw(ctx) {
     
     background.draw(ctx);
     player.draw(ctx);
+    enemies.forEach(function(enemy) {enemy.draw(ctx);});
     bullets.forEach(function(bull) {bull.draw(ctx);});
     ctx.font = "24pt sans-serif";
     ctx.fillText(Math.round(1000/deltaDraw) + " fps",20,40);
     ctx.font = "10pt sans-serif";
-    ctx.fillText("Last update: bullets added (shoot with space)",20,60);
+    ctx.fillText("Last update: moving enemies added (no collision detection)",20,60);
 }
